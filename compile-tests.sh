b3245843b7657fa4131aaa1f91775111a5715a7c
damunguiae <damunguia@proton.me>
2025-11-23 13:12:07 -0600

    Update compile-tests.sh to compile all Java files in the ds and test directories

--- compile-tests.sh
+++ compile-tests.sh
@@ -2,10 +2,10 @@
 # Compile and run JUnit tests
 
 # Compile source code
-javac -d out ds/linear/linkedList/*.java
+javac -d out ds/*/**/*.java
 
 # Compile tests
-javac -cp "lib/junit-platform-console-standalone-1.10.1.jar:out" -d out test/ds/linear/linkedList/*.java
+javac -cp "lib/junit-platform-console-standalone-1.10.1.jar:out" -d out test/ds/*/**/*.java
 
 # Run tests
 java -jar lib/junit-platform-console-standalone-1.10.1.jar --class-path out --scan-class-path
